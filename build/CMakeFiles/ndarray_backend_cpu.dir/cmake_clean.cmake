aaccb08fa8672e16666faed6eef252732eae47d6
LightString <hjing7@wisc.edu>
2024-08-07 11:11:48 -0400

    Refactor code for improved performance

---
+++ build/CMakeFiles/ndarray_backend_cpu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/hyjing/Code/DeepLearningSystem/HW3/python/needle/backend_ndarray/ndarray_backend_cpu.cpython-312-x86_64-linux-gnu.so"
+  "/home/hyjing/Code/DeepLearningSystem/HW3/python/needle/backend_ndarray/ndarray_backend_cpu.pdb"
+  "CMakeFiles/ndarray_backend_cpu.dir/src/ndarray_backend_cpu.cc.o"
+  "CMakeFiles/ndarray_backend_cpu.dir/src/ndarray_backend_cpu.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ndarray_backend_cpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
